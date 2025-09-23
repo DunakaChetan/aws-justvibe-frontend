0e89926910ca92ef175ec95104377e7da6fddbf6
DunakaChetan <thedevil521230@gmail.com>
2025-09-23 23:12:41 +0530

    first commit

---
+++ src/SongsData.js
@@ -0,0 +1,146 @@
+export const albums = [
+  {
+      id: 'GunturKaaram',
+      title: 'Guntur Kaaram',
+      artist: 'Thaman S',
+      img: '/AlbumImages/GunturKaaram.jpg',
+      category: 'Action', // Added category
+      songs: [
+          { title: 'Guntur Kaaram', src: '/Music/Guntur Kaaram/Guntur Kaaram.mp3', img: '/AlbumImages/GunturKaaram.jpg' },
+          { title: 'Kurchi Madathapetti', src: '/Music/Guntur Kaaram/Kurchi Madathapetti.mp3', img: '/AlbumImages/GunturKaaram.jpg' },
+          { title: 'Dum Masala', src: '/Music/Guntur Kaaram/Dum Masala.mp3', img: '/AlbumImages/GunturKaaram.jpg' },
+          { title: 'Oh My Baby', src: '/Music/Guntur Kaaram/Oh My Baby.mp3', img: '/AlbumImages/GunturKaaram.jpg' },
+          { title: 'Mawaa Enthaina', src: '/Music/Guntur Kaaram/Mawaa Enthaina.mp3', img: '/AlbumImages/GunturKaaram.jpg' },
+          { title: 'Ramana Aei', src: '/Music/Guntur Kaaram/Ramana Aei.mp3', img: '/AlbumImages/GunturKaaram.jpg' },
+          { title: 'Amma', src: '/Music/Guntur Kaaram/Amma.mp3', img: '/AlbumImages/GunturKaaram.jpg' }
+      ]
+  },
+  {
+      id: 'Devara',
+      title: 'Devara',
+      artist: 'Anirudh Ravichander',
+      img: '/AlbumImages/Devara1.jpg',
+      category: 'Action', // Added category
+      songs: [
+          { title: 'Fear', src: '/Music/Devara/Fear.mp3', img: '/AlbumImages/Devara1.jpg' },
+          { title: 'Chuttamalle', src: '/Music/Devara/Chuttamalle.mp3', img: '/AlbumImages/Devara1.jpg' },
+          { title: 'Daavudi', src: '/Music/Devara/Daavudi.mp3', img: '/AlbumImages/Devara1.jpg' },
+          { title: 'Ayudha Pooja', src: '/Music/Devara/Ayudha Pooja.mp3', img: '/AlbumImages/Devara1.jpg' },
+          { title: 'Red Sea', src: '/Music/Devara/Red Sea.mp3', img: '/AlbumImages/Devara1.jpg' },
+          { title: 'All Hail The Tiger', src: '/Music/Devara/All Hail The Tiger.mp3', img: '/AlbumImages/Devara1.jpg' }
+      ]
+  },
+  {
+      id: 'GameChanger',
+      title: 'Game Changer',
+      artist: 'Thaman S',
+      img: '/AlbumImages/GameChanger1.jpg',
+      category: 'Action', // Added category
+      songs: [
+          { title: 'Jaragandi', src: '/Music/Game Changer/Jaragandi.mp3', img: '/AlbumImages/GameChanger1.jpg' },
+          { title: 'Raa Macha Macha', src: '/Music/Game Changer/Raa Macha Macha.mp3', img: '/AlbumImages/GameChanger1.jpg' },
+          { title: 'Naanaa Hyraanaa', src: '/Music/Game Changer/Naanaa Hyraanaa.mp3', img: '/AlbumImages/GameChanger1.jpg' },
+          { title: 'Dhop', src: '/Music/Game Changer/Dhop.mp3', img: '/AlbumImages/GameChanger1.jpg' },
+          { title: 'Arugu Meedha', src: '/Music/Game Changer/Arugu Meedha.mp3', img: '/AlbumImages/GameChanger1.jpg' },
+          { title: 'Konda Devara', src: '/Music/Game Changer/Konda Devara.mp3', img: '/AlbumImages/GameChanger1.jpg' },
+          { title: 'Koparap', src: '/Music/Game Changer/Koparap.mp3', img: '/AlbumImages/GameChanger1.jpg' },
+          { title: 'Unpredictable', src: '/Music/Game Changer/Unpredictable.mp3', img: '/AlbumImages/GameChanger1.jpg' }
+      ]
+  },
+  {
+      id: 'Pushpa2',
+      title: 'Pushpa 2',
+      artist: 'Devi Sri Prasad',
+      img: '/AlbumImages/Pushpa2.jpg',
+      category: 'Action', // Added category
+      songs: [
+          { title: 'Pushpa Pushpa', src: '/Music/Pushpa 2/Pushpa Pushpa.mp3', img: '/AlbumImages/Pushpa2.jpg' },
+          { title: 'Sooseki', src: '/Music/Pushpa 2/Sooseki.mp3', img: '/AlbumImages/Pushpa2.jpg' },
+          { title: 'Kissik', src: '/Music/Pushpa 2/Kissik.mp3', img: '/AlbumImages/Pushpa2.jpg' },
+          { title: 'Peelings', src: '/Music/Pushpa 2/Peelings.mp3', img: '/AlbumImages/Pushpa2.jpg' },
+          { title: 'Gango Renuka Thalli', src: '/Music/Pushpa 2/Gango Renuka Thalli.mp3', img: '/AlbumImages/Pushpa2.jpg' },
+          { title: 'Dammunte Pattukora', src: '/Music/Pushpa 2/Dammunte Pattukora.mp3', img: '/AlbumImages/Pushpa2.jpg' },
+      ]
+  },
+  {
+      id: 'OG',
+      title: 'OG',
+      artist: 'Thaman S',
+      img: '/AlbumImages/OG.jpg',
+      category: 'Action', // Added category
+      songs: [
+          { title: 'Hungry Cheetah', src: '/Music/OG/Hungry Cheetah.mp3', img: '/AlbumImages/OG.jpg' },
+      ]
+  },
+  {
+      id: 'ArjunReddy',
+      title: 'Arjun Reddy',
+      artist: 'Radhan & Harshvardhan Rameshwar',
+      img: '/AlbumImages/ArjunReddy.jpg',
+      category: 'Romantic', // Added category
+      songs: [
+          { title: 'Dhooram', src: '/Music/Arjun Reddy/Dhooram.mp3', img: '/AlbumImages/ArjunReddy.jpg' },
+          { title: 'Telisiney Na Nuvvey', src: '/Music/Arjun Reddy/Telisiney Na Nuvvey.mp3', img: '/AlbumImages/ArjunReddy.jpg' },
+          { title: 'Emitemitemo', src: '/Music/Arjun Reddy/Emitemitemo.mp3', img: '/AlbumImages/ArjunReddy.jpg' },
+          { title: 'Madhurame', src: '/Music/Arjun Reddy/Madhurame.mp3', img: '/AlbumImages/ArjunReddy.jpg' },
+          { title: 'Mari Mari', src: '/Music/Arjun Reddy/Mari Mari.mp3', img: '/AlbumImages/ArjunReddy.jpg' },
+          { title: 'Oopiri Aaguthunnadey', src: '/Music/Arjun Reddy/Oopiri Aaguthunnadey.mp3', img: '/AlbumImages/ArjunReddy.jpg' },
+          { title: 'Gundelonaa', src: '/Music/Arjun Reddy/Gundelonaa.mp3', img: '/AlbumImages/ArjunReddy.jpg' }
+      ]
+  },
+  {
+      id: 'KGF2',
+      title: 'KGF 2',
+      artist: 'Ravi Basrur',
+      img: '/AlbumImages/KGF2.jpg',
+      category: 'Action', // Added category
+      songs: [
+          { title: 'Toofan', src: '/Music/KGF 2/Toofan.mp3', img: '/AlbumImages/KGF2.jpg' },
+          { title: 'Yadagara Yadagara', src: '/Music/KGF 2/Yadagara Yadagara.mp3', img: '/AlbumImages/KGF2.jpg' },
+          { title: 'Sultan', src: '/Music/KGF 2/Sulthana.mp3', img: '/AlbumImages/KGF2.jpg' },
+          { title: 'Mehabooba', src: '/Music/KGF 2/Mehabooba.mp3', img: '/AlbumImages/KGF2.jpg' },
+          { title: 'The Monster', src: '/Music/KGF 2/The Monster.mp3', img: '/AlbumImages/KGF2.jpg' }
+      ]
+  },
+  {
+      id: 'LoveYourVoice',
+      title: 'Love Your Voice',
+      artist: 'JONY',
+      img: '/AlbumImages/LoveYourVoice.jpg',
+      category: 'Romantic', // Added category
+      songs: [
+          { title: 'JONY - Love your voice', src: '/Music/Love Your Voice/JONY - Love your voice.mp3', img: '/AlbumImages/LoveYourVoice.jpg' }
+      ]
+  },
+  {
+      id: 'ShapeOfYou',
+      title: 'Shape Of You',
+      artist: 'Ed Sheeran',
+      img: '/AlbumImages/ShapeOfYou.jpg',
+      category: 'Happy', // Added category
+      songs: [
+          { title: 'Ed Sheeran - Shape of You', src: '/Music/Shape Of You/Ed Sheeran - Shape of You.mp3', img: '/AlbumImages/ShapeOfYou.jpg' }
+      ]
+  },
+  {
+      id: 'Perfect',
+      title: 'Perfect',
+      artist: 'Ed Sheeran',
+      img: '/AlbumImages/Perfect.jpg',
+      category: 'Romantic', // Added category
+      songs: [
+          { title: 'Perfect', src: '/Music/Perfect/Perfect.mp3', img: '/AlbumImages/Perfect.jpg' }
+      ]
+  },
+  {
+      id: 'Die With A Smile',
+      title: 'Die With A Smile',
+      artist: 'Lady Gaga and Bruno Mars',
+      img: '/AlbumImages/Die With A Smile.jpg',
+      category: 'Romantic', // Added category
+      songs: [
+          { title: 'Die With A Smile', src: '/Music/Die With A Smile/Die With A Smile.mp3', img: '/AlbumImages/Die With A Smile.jpg' }
+      ]
+  }
+  
+];
